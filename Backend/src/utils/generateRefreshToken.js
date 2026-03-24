7f27e5ca02a2605730dfc5654799661f88824ff7
PrabinKK16 <prabinkumarkhamania7@gmail.com>
2026-03-24 20:55:45 +0530

    Add initial backend setup with environment variables and utility functions for API error handling, responses, and token generation

---
+++ Backend/src/utils/generateRefreshToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
